73fe708cd6091d1a17afd494c98c447e30d22501
ssonumkar <moreanjani022@gmail.com>
2025-07-17 19:57:14 +0200

    First Commit

---
+++ src/main/java/com/swisscom/api/ServiceManagerApiApplication.java
@@ -0,0 +1,15 @@
+package com.swisscom.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceManagerApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ServiceManagerApiApplication.class, args);
+
+	}
+
+}
