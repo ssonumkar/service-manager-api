91226e0513a96489f76c3d33c9a30781b7bea22f
Sagar Sonumkar <47482125+ssonumkar@users.noreply.github.com>
2025-07-20 17:50:04 +0200

    Update run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#echo "Java 17+, Maven 3 and Mongodb should be pre-installed"
 # Clean and build the project
 echo "Building Spring Boot application..."
 ./mvnw clean install -DskipTests || { echo "Build failed"; exit 1; }
