4f593d5a961c606039f0dd8c918cfaccce5901ee
sagar <Sagar.Sonumkar@iwhalecloud.com>
2025-07-20 14:15:36 +0200

    Added a ValidationService to handle validations

---
+++ src/main/java/com/swisscom/api/utils/ValidationService.java
@@ -0,0 +1,34 @@
+package com.swisscom.api.utils;
+
+import com.swisscom.api.dao.ResourceRepository;
+import com.swisscom.api.dao.SwisscomServiceRepository;
+import com.swisscom.api.service.ResourceService;
+import com.swisscom.api.service.SwisscomServiceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ValidationService implements IValidationService {
+    private final SwisscomServiceRepository swisscomServiceRepository;
+    private final ResourceRepository resourceRepository;
+
+    @Override
+    public boolean isServiceValid(String serviceId) {
+        return swisscomServiceRepository.findById(serviceId).isPresent();
+    }
+
+    @Override
+    public boolean isResourceValid(String resourceId) {
+        return resourceRepository.findById(resourceId).isPresent();
+    }
+
+    public void validateServiceAndResource(String serviceId, String resourceId) {
+        if (!isServiceValid(serviceId)) {
+            throw new IllegalArgumentException("Invalid service ID: " + serviceId);
+        }
+        if (!isResourceValid(resourceId)) {
+            throw new IllegalArgumentException("Invalid resource ID: " + resourceId);
+        }
+    }
+}
